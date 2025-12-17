037f0a91b05d1ed236b335c187530b0428996c04
Ulitin <akira-ul@yandex.ru>
2025-12-17 16:27:33 +0300

    modify

--- point.js
+++ point.js
@@ -1,16 +1,21 @@
 /**
- * Класс получает бонусные баллы и выводит его в сущности (контакт, сделка, финансовая краточка).
+ * Класс для работы с бонусными баллами в CRM-сущностях.
+ * Позволяет получать актуальную информацию о бонусном счёте клиента
+ * и отображать её в карточках различных сущностей (контакт, сделка, финансовая карточка).
+ * Поддерживает работу с двумя программами лояльности: MR и Imperia.
  */
 class BrsPoint {
 	
-	#entityCode = 0; // код сущности
-	#entityId = 0; // идентификатор сущности
-	#pointEis = false;
-	#entityClass = false; // класс или идентификатор для вставки формы
-	#settings = false; // массив дополнительных параметров
+	#entityCode = 0; // код типа сущности в CRM (например: контакт, сделка)
+	#entityId = 0; // уникальный идентификатор конкретной сущности
+	#pointEis = false; // флаг, указывающий, что данные уже были загружены хотя бы раз
+	#entityClass = false; // CSS-селектор или класс элемента для вставки формы с баллами
+	#settings = false; // объект с дополнительными параметрами и callback-функциями
 	
 	/**
-	 * Получаем актуальный баланс бонусного счёта и выводим его в DOM узел.
+	 * Инициализирует класс для работы с бонусными баллами.
+	 * Создаёт форму отображения баллов в DOM, загружает актуальные данные
+	 * и настраивает обработчики событий для обновления информации.
 	 */
 	constructor(entityCode, entityId, entityClass, settings){
 		
@@ -21,58 +26,72 @@ class BrsPoint {
 		
 		this.#pointEis = false;
 		
-		this.renderSection(); // формируем DOM формы "Информация по баллам"
+		this.renderSection(); // формируем DOM-структуру формы "Информация по баллам"
 		
-		this.getCountPoint(); // получаем актуальный баланс бонусного счёта
+		this.getCountPoint(); // выполняем первичную загрузку баланса бонусного счёта
 		
-		this.#pointEis = true;
+		this.#pointEis = true; // отмечаем, что первичная загрузка выполнена
 		
+		// привязываем обработчик клика на кнопку "Обновить" для повторной загрузки данных
 		$('.entityPointViewCountPoint').on('click', this.getCountPoint.bind(this));
 		
 	}
 	
 	/**
-	 * Метод получает актуальный баланс бонусного счёта.
+	 * Получает актуальный баланс бонусного счёта через API.
+	 * Отправляет AJAX-запрос к контроллеру модуля "brs.point" и обновляет
+	 * отображаемые значения баллов в DOM. При ошибке показывает сообщение об ошибке.
 	 */
 	getCountPoint(event){
 		
+		// предотвращаем стандартное поведение браузера, если метод вызван по событию
 		if(typeof event != 'undefined'){
 			event.preventDefault();
 		}
 		
+		// блокируем кнопку обновления на время выполнения запроса
 		$('.entityPointViewCountPoint').attr('disabled', 'disabled');
 		
 		// отправляем запрос в контроллер получения баллов модуля "brs.point"
 		BX.ajax.runAction('brs:point.api.PointController.entity', {
 			data: {
-				entityCode: this.#entityCode,
-				entityId: this.#entityId,
-				pointEis: this.#pointEis
+				entityCode: this.#entityCode, // передаём код типа сущности
+				entityId: this.#entityId, // передаём ID сущности
+				pointEis: this.#pointEis // передаём флаг повторной загрузки
 			}
 		}).then(function (response) {
 		
+			// разблокируем кнопку обновления после получения ответа
 			$('.entityPointViewCountPoint').removeAttr('disabled', 'disabled');
 			
+			// скрываем блок с ошибками, если он был показан ранее
 			$('.crm_entity_point_error_block').hide(0);
 		
+			// если данные не получены, прерываем выполнение
 			if(response.data.length === 0){
 				return;
 			}
 			
+			// вызываем callback успешного получения данных, если он определён
 			this.eventSuccessPointAction(response);
 			
+			// обновляем отображаемые значения баллов программы MR
 			$('.crm_entity_field_point_mr').text(response.data.MR);
 			$('.crm_entity_field_point_mr_rub').text(response.data.MR_RUB);
 			
+			// обновляем отображаемые значения баллов программы Imperia
 			$('.crm_entity_field_point_imperia').text(response.data.IMPERIA);
 			$('.crm_entity_field_point_imperia_rub').text(response.data.IMPERIA_RUB);
 			
 		}.bind(this), (response) => {
 		
+			// разблокируем кнопку обновления после ошибки
 			$('.entityPointViewCountPoint').removeAttr('disabled', 'disabled');
 			
+			// показываем блок с сообщением об ошибке
 			$('.crm_entity_point_error_block').show(0);
 			
+			// выводим текст ошибки, полученный от сервера
 			$('.crm_entity_point_error_block_message').text(response.errors[0].message);
 			
 		});
@@ -82,56 +101,137 @@ class BrsPoint {
 	}
 	
 	/**
-	 * Событие успешного получения баллов
+	 * Вызывает пользовательский callback при успешном получении баллов.
+	 * Позволяет внешнему коду реагировать на обновление данных о баллах.
 	 */
 	eventSuccessPointAction(response){
 		
+		// проверяем, переданы ли настройки при инициализации класса
 		if(typeof this.#settings == 'undefined'){
 			return false;
 		}
 		
+		// проверяем, определена ли callback-функция в настройках
 		if(typeof this.#settings.eventSuccessPointAction == 'undefined'){
 			return false;
 		}
 		
+		// вызываем пользовательскую callback-функцию с данными ответа
 		this.#settings.eventSuccessPointAction(response);
 		
 	}
 	
 	/**
-	 * Выводим в DOM форму с балансом бонусного счёта.
+	 * Формирует и вставляет в DOM HTML-разметку формы с информацией о баллах.
+	 * Создаёт секцию с полями для отображения баллов MR и Imperia (в баллах и рублях),
+	 * блок для сообщений об ошибках и кнопку обновления данных.
 	 */
 	renderSection(){
 		
-		var html = `<div class="ui-entity-editor-section" data-cid="user_v0cwaljr1"><div class="ui-entity-editor-section-header"><div class="ui-entity-editor-header-title"><span class="ui-entity-editor-header-title-text">Информация по баллам</span><input class="ui-entity-editor-header-title-text" style="display: none;"></div></div><div class="ui-entity-editor-section-content"><div class="ui-entity-editor-content-block" style="margin-bottom: 0px;">
-
-<div class="row" style="display: flex;">
-<div class="ui-entity-editor-content-block ui-entity-editor-content-block-field-custom-date" style="flex: 0 50%;"><div class="ui-entity-editor-block-before-action"></div><div class="ui-entity-editor-block-draggable-btn-container"></div><div class="ui-entity-editor-block-title"><label class="ui-entity-editor-block-title-text">Баллов MP</label></div><div class="ui-entity-editor-content-block"><span class="fields date field-wrap"><span class="fields date field-item crm_entity_field_point_mr" id="crm_entity_field_point_mr">0</span></span></div></div>
-<div class="ui-entity-editor-content-block ui-entity-editor-content-block-field-custom-date" style="flex: 0 50%;"><div class="ui-entity-editor-block-before-action"></div><div class="ui-entity-editor-block-draggable-btn-container"></div><div class="ui-entity-editor-block-title"><label class="ui-entity-editor-block-title-text">Баллы MP в рублях</label></div><div class="ui-entity-editor-content-block"><span class="fields date field-wrap"><span class="fields date field-item crm_entity_field_point_mr_rub">0</span></span></div></div>
-</div>
-<div class="row" style="display: flex; margin-top: -13px;">
-<div class="ui-entity-editor-content-block ui-entity-editor-content-block-field-custom-date" style="flex: 0 50%; margin: 0px 0px 0px;"><div class="ui-entity-editor-block-before-action"></div><div class="ui-entity-editor-block-draggable-btn-container"></div><div class="ui-entity-editor-block-title"><label class="ui-entity-editor-block-title-text">Баллов Imperia</label></div><div class="ui-entity-editor-content-block"><span class="fields date field-wrap"><span class="fields date field-item crm_entity_field_point_imperia">0</span></span></div></div>
-<div class="ui-entity-editor-content-block ui-entity-editor-content-block-field-custom-date" style="flex: 0 50%; margin: 0px 0px 0px;"><div class="ui-entity-editor-block-before-action"></div><div class="ui-entity-editor-block-draggable-btn-container"></div><div class="ui-entity-editor-block-title"><label class="ui-entity-editor-block-title-text">Баллы Imperia в рублях</label></div><div class="ui-entity-editor-content-block"><span class="fields date field-wrap"><span class="fields date field-item crm_entity_field_point_imperia_rub">0</span></span></div></div>
-</div>
-
-<!-- .ui-alert.ui-alert-danger-->
-<div class="ui-alert ui-alert-danger crm_entity_point_error_block" style="display: none;">
-    <span class="ui-alert-message"><strong>Ошибка!</strong> <span class="crm_entity_point_error_block_message">Текст предупреждения находится здесь.</span></span>
-</div>
-
-<div class="row" style="display: flex;">
-	<button class="ui-btn ui-btn-xs ui-btn-primary entityPointViewCountPoint">Обновить</button>
-</div>
-
-</div></div></div>`;
+		// HTML-разметка секции с информацией о бонусных баллах
+		var html = `
+			<div class="ui-entity-editor-section" data-cid="user_v0cwaljr1">
+				<!-- Заголовок секции -->
+				<div class="ui-entity-editor-section-header">
+					<div class="ui-entity-editor-header-title">
+						<span class="ui-entity-editor-header-title-text">Информация по баллам</span>
+						<input class="ui-entity-editor-header-title-text" style="display: none;">
+					</div>
+				</div>
+				
+				<!-- Контент секции с полями баллов -->
+				<div class="ui-entity-editor-section-content">
+					<div class="ui-entity-editor-content-block" style="margin-bottom: 0px;">
+						
+						<!-- Первая строка: баллы MR -->
+						<div class="row" style="display: flex;">
+							<!-- Поле: количество баллов MR -->
+							<div class="ui-entity-editor-content-block ui-entity-editor-content-block-field-custom-date" style="flex: 0 50%;">
+								<div class="ui-entity-editor-block-before-action"></div>
+								<div class="ui-entity-editor-block-draggable-btn-container"></div>
+								<div class="ui-entity-editor-block-title">
+									<label class="ui-entity-editor-block-title-text">Баллов MP</label>
+								</div>
+								<div class="ui-entity-editor-content-block">
+									<span class="fields date field-wrap">
+										<span class="fields date field-item crm_entity_field_point_mr" id="crm_entity_field_point_mr">0</span>
+									</span>
+								</div>
+							</div>
+							
+							<!-- Поле: баллы MR в рублях -->
+							<div class="ui-entity-editor-content-block ui-entity-editor-content-block-field-custom-date" style="flex: 0 50%;">
+								<div class="ui-entity-editor-block-before-action"></div>
+								<div class="ui-entity-editor-block-draggable-btn-container"></div>
+								<div class="ui-entity-editor-block-title">
+									<label class="ui-entity-editor-block-title-text">Баллы MP в рублях</label>
+								</div>
+								<div class="ui-entity-editor-content-block">
+									<span class="fields date field-wrap">
+										<span class="fields date field-item crm_entity_field_point_mr_rub">0</span>
+									</span>
+								</div>
+							</div>
+						</div>
+						
+						<!-- Вторая строка: баллы Imperia -->
+						<div class="row" style="display: flex; margin-top: -13px;">
+							<!-- Поле: количество баллов Imperia -->
+							<div class="ui-entity-editor-content-block ui-entity-editor-content-block-field-custom-date" style="flex: 0 50%; margin: 0px 0px 0px;">
+								<div class="ui-entity-editor-block-before-action"></div>
+								<div class="ui-entity-editor-block-draggable-btn-container"></div>
+								<div class="ui-entity-editor-block-title">
+									<label class="ui-entity-editor-block-title-text">Баллов Imperia</label>
+								</div>
+								<div class="ui-entity-editor-content-block">
+									<span class="fields date field-wrap">
+										<span class="fields date field-item crm_entity_field_point_imperia">0</span>
+									</span>
+								</div>
+							</div>
+							
+							<!-- Поле: баллы Imperia в рублях -->
+							<div class="ui-entity-editor-content-block ui-entity-editor-content-block-field-custom-date" style="flex: 0 50%; margin: 0px 0px 0px;">
+								<div class="ui-entity-editor-block-before-action"></div>
+								<div class="ui-entity-editor-block-draggable-btn-container"></div>
+								<div class="ui-entity-editor-block-title">
+									<label class="ui-entity-editor-block-title-text">Баллы Imperia в рублях</label>
+								</div>
+								<div class="ui-entity-editor-content-block">
+									<span class="fields date field-wrap">
+										<span class="fields date field-item crm_entity_field_point_imperia_rub">0</span>
+									</span>
+								</div>
+							</div>
+						</div>
+						
+						<!-- Блок для отображения ошибок (по умолчанию скрыт) -->
+						<div class="ui-alert ui-alert-danger crm_entity_point_error_block" style="display: none;">
+							<span class="ui-alert-message">
+								<strong>Ошибка!</strong> 
+								<span class="crm_entity_point_error_block_message">Текст предупреждения находится здесь.</span>
+							</span>
+						</div>
+						
+						<!-- Кнопка обновления баланса баллов -->
+						<div class="row" style="display: flex;">
+							<button class="ui-btn ui-btn-xs ui-btn-primary entityPointViewCountPoint">Обновить</button>
+						</div>
+						
+					</div>
+				</div>
+			</div>
+		`;
 		
+		// вставляем HTML в DOM в зависимости от настроек
 		if(this.#entityClass == false){
+			// если селектор не указан, вставляем после главной секции редактора сущности
 			$('.ui-entity-editor-column-content .ui-entity-editor-section[data-cid="main"]').after(html);
 		} else {
+			// если селектор указан, вставляем в указанный элемент
 			$(this.#entityClass).html(html);
 		}
 		
 	}
 	
 }
-
--- subscribe.js
+++ subscribe.js
@@ -1,39 +1,66 @@
 /**
- * Класс получает бонусные баллы и выводит его в сущности (контакт, сделка, финансовая краточка).
+ * Класс для работы с подписками БРС (Бонусная Расчетная Система).
+ * Отвечает за получение списка подписок контакта через API и отображение
+ * их в интерфейсе Bitrix24 в виде двух таблиц: активные и неактивные подписки.
+ * 
+ * Класс автоматически встраивается в карточку контакта и формирует 
+ * отдельную секцию с информацией о подписках.
  */
 class BrsSubscribe {
 	
-	#contactId = 0; // идентификатор контакта
+	// Приватное свойство для хранения ID контакта, с которым работает класс
+	#contactId = 0;
 	
 	/**
-	 * Получаем подписки и выводим его в DOM узел.
+	 * Конструктор класса инициализирует работу с подписками контакта.
+	 * 
+	 * Выполняет следующие действия:
+	 * 1. Сохраняет ID контакта в приватное свойство
+	 * 2. Рендерит секцию формы в DOM-дереве страницы
+	 * 3. Загружает список подписок с сервера и отображает их
+	 * 
+	 * contactId - числовой идентификатор контакта в системе Bitrix24
 	 */
 	constructor(contactId){
 		
 		this.#contactId = contactId;
 		
-		this.renderSection(); // формируем DOM формы "Информация по баллам"
+		// Создаем структуру секции "Подписки БРС" в карточке контакта
+		this.renderSection();
 		
-		this.getSubscribes(); // получаем подписки и устанавливаем в форму
+		// Отправляем запрос на получение подписок и заполняем таблицы данными
+		this.getSubscribes();
 		
 	}
 	
 	/**
-	 * Метод получает список подписок и устанавливает в форму.
+	 * Метод получает список подписок контакта с сервера через AJAX.
+	 * 
+	 * Отправляет запрос к контроллеру SubscribeController.get, передавая ID контакта.
+	 * При успешном получении данных вызывает метод eventSuccessSubscribeAction для
+	 * отображения подписок в интерфейсе.
+	 * 
+	 * Если подписок нет (пустой массив), удаляет всю секцию из DOM.
+	 * 
+	 * event - объект события (опционально), используется для отмены стандартного поведения
 	 */
 	getSubscribes(event){
 		
+		// Если метод вызван из обработчика события, предотвращаем стандартное действие
 		if(typeof event != 'undefined'){
 			event.preventDefault();
 		}
 		
-		// отправляем запрос в контроллер получения подписок
+		// Выполняем AJAX-запрос к API контроллеру для получения списка подписок
+		// Используем встроенный механизм Bitrix24 для работы с AJAX
 		BX.ajax.runAction('brs:main.api.SubscribeController.get', {
 			data: {
-				contactId: this.#contactId,
+				contactId: this.#contactId, // Передаем ID контакта для фильтрации подписок
 			}
 		}).then(function (response) {
 		
+			// Если сервер вернул пустой массив, значит у контакта нет подписок
+			// Удаляем секцию из интерфейса, чтобы не показывать пустой блок
 			if(response.data.length === 0){
 				
 				$('#contactSubscribeList').remove();
@@ -42,95 +69,188 @@ class BrsSubscribe {
 				
 			}
 			
+			// Передаем полученные данные в метод для отображения
 			this.eventSuccessSubscribeAction(response.data);
 			
-		}.bind(this));
+		}.bind(this)); // Привязываем контекст this к классу
 		
 		return false;
 		
 	}
 	
 	/**
-	 * Событие успешного получения подписок
+	 * Обработчик успешного получения подписок с сервера.
+	 * 
+	 * Метод разделяет подписки на две категории:
+	 * - Активные (IS_ACTIVE === '1')
+	 * - Неактивные (IS_ACTIVE !== '1')
+	 * 
+	 * Для каждой категории формирует HTML-таблицу с данными:
+	 * - Номер договора (CONTRACT_NUMBER)
+	 * - Название пакета (PACKAGE_NAME)
+	 * - Дата начала действия (DATE_START)
+	 * - Дата окончания действия (DATE_FINISH)
+	 * 
+	 * Если в категории нет подписок, выводится текст "Нет".
+	 * 
+	 * subscribes - массив объектов с данными подписок, полученный от сервера
 	 */
 	eventSuccessSubscribeAction(subscribes){
 		
-		let active = '';
-		let inactive = '';
+		// Переменные для накопления HTML-кода строк таблиц
+		let active = '';   // HTML строк активных подписок
+		let inactive = ''; // HTML строк неактивных подписок
 		
+		// Проходим по всем подпискам и распределяем их по категориям
 		for(let key in subscribes){
+			
+			// Формируем строку таблицы с данными подписки
+			const tableRow = `
+				<tr>
+					<td>${subscribes[key].CONTRACT_NUMBER}</td>
+					<td>${subscribes[key].PACKAGE_NAME}</td>
+					<td>${subscribes[key].DATE_START}</td>
+					<td>${subscribes[key].DATE_FINISH}</td>
+				</tr>
+			`;
+			
+			// Распределяем по категориям в зависимости от статуса активности
 			if(subscribes[key].IS_ACTIVE === '1'){
-				active += '<tr><td>' + subscribes[key].CONTRACT_NUMBER + '</td><td>' + subscribes[key].PACKAGE_NAME + '</td><td>' + subscribes[key].DATE_START + '</td><td>' + subscribes[key].DATE_FINISH + '</td></tr>';
+				active += tableRow;
 			} else {
-				inactive += '<tr><td>' + subscribes[key].CONTRACT_NUMBER + '</td><td>' + subscribes[key].PACKAGE_NAME + '</td><td>' + subscribes[key].DATE_START + '</td><td>' + subscribes[key].DATE_FINISH + '</td></tr>';
+				inactive += tableRow;
 			}
 		}
 		
-		// если значения пустые, то устанавливаем значение "Нет"
+		// Если активных подписок нет, устанавливаем текст "Нет"
 		if(active === ''){
 			active = 'Нет';
 		} else {
-			active = `<table class="brs-contact-subscribe-table">
-	<thead>
-		<th>№ договора</th>
-		<th>Пакет</th>
-		<th>Дата начала</th>
-		<th>Дата окончания</th>
-	</thead>
-	<tbody>
-		` + active + `
-	</tbody>
-</table>`;
+			// Формируем полную HTML-таблицу с заголовками и накопленными строками
+			active = `
+				<table class="brs-contact-subscribe-table">
+					<thead>
+						<th>№ договора</th>
+						<th>Пакет</th>
+						<th>Дата начала</th>
+						<th>Дата окончания</th>
+					</thead>
+					<tbody>
+						${active}
+					</tbody>
+				</table>
+			`;
 		}
 		
+		// Если неактивных подписок нет, устанавливаем текст "Нет"
 		if(inactive === ''){
 			inactive = 'Нет';
 		} else {
-			inactive = `<table class="brs-contact-subscribe-table">
-	<thead>
-		<th>№ договора</th>
-		<th>Пакет</th>
-		<th>Дата начала</th>
-		<th>Дата окончания</th>
-	</thead>
-	<tbody>
-		` + inactive + `
-	</tbody>
-</table>`;
+			// Формируем полную HTML-таблицу с заголовками и накопленными строками
+			inactive = `
+				<table class="brs-contact-subscribe-table">
+					<thead>
+						<th>№ договора</th>
+						<th>Пакет</th>
+						<th>Дата начала</th>
+						<th>Дата окончания</th>
+					</thead>
+					<tbody>
+						${inactive}
+					</tbody>
+				</table>
+			`;
 		}
 		
+		// Вставляем сформированный HTML в соответствующие контейнеры на странице
 		$('#contactSubscribeListActive').html(active);
 		$('#contactSubscribeListInactive').html(inactive);
 		
 	}
 	
 	/**
-	 * Выводим в DOM форму с подписками.
+	 * Метод создает и встраивает в DOM секцию "Подписки БРС".
+	 * 
+	 * Формирует HTML-структуру секции в стиле стандартных секций Bitrix24:
+	 * - Заголовок секции "Подписки БРС"
+	 * - Два блока для отображения таблиц (активные и неактивные подписки)
+	 * - Базовые стили для оформления таблиц
+	 * 
+	 * Секция вставляется после основной секции карточки контакта (data-cid="main")
+	 * в правильное место DOM-дерева для корректного отображения в интерфейсе.
 	 */
 	renderSection(){
 		
-		var html = `<div id="contactSubscribeList" class="ui-entity-editor-section" data-cid="user_v0cwaljr1"><div class="ui-entity-editor-section-header"><div class="ui-entity-editor-header-title"><span class="ui-entity-editor-header-title-text">Подписки БРС</span><input class="ui-entity-editor-header-title-text" style="display: none;"></div></div><div class="ui-entity-editor-section-content"><div class="ui-entity-editor-content-block" style="margin-bottom: 0px;">
-
-<div class="row" style="display: flex;">
-<div class="ui-entity-editor-content-block ui-entity-editor-content-block-field-custom-date" style="flex: 0 100%; min-width: 100%;"><div class="ui-entity-editor-block-before-action"></div><div class="ui-entity-editor-block-draggable-btn-container"></div><div class="ui-entity-editor-block-title"><label class="ui-entity-editor-block-title-text">Действующие</label></div><div class="ui-entity-editor-content-block" id="contactSubscribeListActive">
-</div></div>
-</div>
-
-<div class="row" style="display: flex; margin-top: -13px;">
-<div class="ui-entity-editor-content-block ui-entity-editor-content-block-field-custom-date" style="flex: 0 100%; margin: 0px 0px 0px;"><div class="ui-entity-editor-block-before-action"></div><div class="ui-entity-editor-block-draggable-btn-container"></div><div class="ui-entity-editor-block-title"><label class="ui-entity-editor-block-title-text">Неактивные</label></div><div class="ui-entity-editor-content-block" id="contactSubscribeListInactive"></div></div>
-</div>
-
-</div></div></div>
-
-<style>
-	.brs-contact-subscribe-table { width: 100%; text-align: left; padding: 10px; background-color: #fff; font-size: 13px; margin-top: 10px; border-bottom: 1px solid #e4e4e4; border-radius: 2px; }
-</style>
-
-`;
+		// Формируем HTML-код секции с подписками
+		// Структура повторяет стандартный формат секций редактора сущностей Bitrix24
+		const html = `
+			<div id="contactSubscribeList" class="ui-entity-editor-section" data-cid="user_v0cwaljr1">
+				
+				<!-- Заголовок секции -->
+				<div class="ui-entity-editor-section-header">
+					<div class="ui-entity-editor-header-title">
+						<span class="ui-entity-editor-header-title-text">Подписки БРС</span>
+						<input class="ui-entity-editor-header-title-text" style="display: none;">
+					</div>
+				</div>
+				
+				<!-- Содержимое секции -->
+				<div class="ui-entity-editor-section-content">
+					<div class="ui-entity-editor-content-block" style="margin-bottom: 0px;">
+						
+						<!-- Блок активных подписок -->
+						<div class="row" style="display: flex;">
+							<div class="ui-entity-editor-content-block ui-entity-editor-content-block-field-custom-date" 
+								 style="flex: 0 100%; min-width: 100%;">
+								<div class="ui-entity-editor-block-before-action"></div>
+								<div class="ui-entity-editor-block-draggable-btn-container"></div>
+								<div class="ui-entity-editor-block-title">
+									<label class="ui-entity-editor-block-title-text">Действующие</label>
+								</div>
+								<div class="ui-entity-editor-content-block" id="contactSubscribeListActive">
+									<!-- Здесь будет отображена таблица активных подписок -->
+								</div>
+							</div>
+						</div>
+						
+						<!-- Блок неактивных подписок -->
+						<div class="row" style="display: flex; margin-top: -13px;">
+							<div class="ui-entity-editor-content-block ui-entity-editor-content-block-field-custom-date" 
+								 style="flex: 0 100%; margin: 0px 0px 0px;">
+								<div class="ui-entity-editor-block-before-action"></div>
+								<div class="ui-entity-editor-block-draggable-btn-container"></div>
+								<div class="ui-entity-editor-block-title">
+									<label class="ui-entity-editor-block-title-text">Неактивные</label>
+								</div>
+								<div class="ui-entity-editor-content-block" id="contactSubscribeListInactive">
+									<!-- Здесь будет отображена таблица неактивных подписок -->
+								</div>
+							</div>
+						</div>
+						
+					</div>
+				</div>
+			</div>
+			
+			<!-- Стили для таблиц подписок -->
+			<style>
+				.brs-contact-subscribe-table {
+					width: 100%;
+					text-align: left;
+					padding: 10px;
+					background-color: #fff;
+					font-size: 13px;
+					margin-top: 10px;
+					border-bottom: 1px solid #e4e4e4;
+					border-radius: 2px;
+				}
+			</style>
+		`;
 		
+		// Вставляем сформированный HTML после основной секции карточки контакта
+		// Используем jQuery для поиска и вставки элемента
 		$('.ui-entity-editor-column-content .ui-entity-editor-section[data-cid="main"]').after(html);
 		
 	}
 	
 }
-
