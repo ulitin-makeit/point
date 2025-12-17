d3cfc58d940b14ac46cb900d962b5eea15b7572a
Улитин Евгений <55397251+ulitin-makeit@users.noreply.github.com>
2025-12-17 18:25:52 +0300

    Create class.php

---
+++ local/components/brs.incomingpaymentecomm/payment-manager/class.php
@@ -0,0 +1,126 @@
+<?php
+
+use Bitrix\Crm\ContactTable;
+use Bitrix\Crm\DealTable;
+use Bitrix\Main\Loader;
+use Bitrix\Main\UserTable;
+use Brs\CorporateClients\Models\Orm\CorporateClientsTable;
+use Brs\FinancialCard\Repository\FinancialCard;
+use Brs\Services\DealDate;
+use Brs\Main\Model\Orm\Crm\Contact\ContactPropertyTable;
+use Brs\FinancialCard\Credit;
+
+class DealIncomePaymentManager extends CBitrixComponent
+{
+	private function getClientEmailList(): array
+	{
+		Loader::includeModule('crm');
+		Loader::includeModule('brs.corporateclients');
+
+		$emailList = [];
+
+		$deal = CAllCrmDeal::GetByID($this->arParams['DEAL_ID']);
+		
+		$clientId = $deal['CONTACT_ID'];
+
+		$this->arResult['CONTACT_ID'] = $clientId;
+
+		$dbEmails = CCrmFieldMulti::GetList(
+			[],
+			[
+				'ENTITY_ID' => 'CONTACT',
+				'ELEMENT_ID' => $clientId,
+				'TYPE_ID' => 'EMAIL',
+			]
+		);
+
+		while ($email = $dbEmails->Fetch()) {
+			$emailList[] = $email['VALUE'];
+		}
+
+		return $emailList;
+	}
+
+	private function getDealBeginDate(): ?\Bitrix\Main\Type\Date
+	{
+		$deal = DealTable::getlist(['filter'=>['ID' => $this->arParams['DEAL_ID']], 'select'=>['*', 'UF_*'], 'limit'=>1])->fetch();
+		$dealDate = (new DealDate())->execute($deal);
+		return $dealDate->getStartFieldValue();
+	}
+
+	/**
+	 * Получаем идентификатор клиента кс из свойств контакта.
+	 *
+	 * @return array
+	 */
+	private function getClientOfContact(): array
+	{
+
+		return ContactPropertyTable::getByPrimary($this->arResult['CONTACT_ID'])->Fetch();
+	}
+
+	/**
+	 * Получает компанию, если клиент является корпоративным
+	 * @return ?array
+	 */
+	private function getCorpById(int $clientId = 0): ?array
+	{
+		$dbCorp = CorporateClientsTable::getById($clientId);
+
+		if ($dbCorp->getSelectedRowsCount()) {
+			return $dbCorp->fetch();
+		} else {
+			return null;
+		}
+	}
+
+	/**
+	 * @param $CONTACT_ID
+	 * @return mixed
+	 */
+	public function getClientFullName($CONTACT_ID)
+	{
+		$db = ContactTable::getByPrimary($CONTACT_ID, ['select' => ['FULL_NAME']]);
+		$client = $db->fetch();
+		return $client['FULL_NAME'];
+	}
+
+	public function executeComponent()
+	{
+
+		Loader::includeModule('brs.financialcard');
+
+		$this->arResult['CONTACT_ID'] = 0;
+		$this->arResult['DEAL_ID'] = $this->arParams['DEAL_ID'];
+
+		$this->arResult['EMAIL_LIST'] = $this->getClientEmailList();
+		$this->arResult['CLIENT_PAYMENT'] = FinancialCard::getDealPaymentType($this->arParams['DEAL_ID']);
+		$this->arResult['DEAL_BEGIN_DATE'] = $this->getDealBeginDate();
+
+		$client = $this->getClientOfContact();
+
+		$this->arResult['CLIENT_ID'] = $client[\Brs\Entities\Contact::KS_ID];
+		$this->arResult['CORP'] = $this->getCorpById((int) $client[\Brs\Entities\Contact::MOM_PLACE_WORK]);
+
+		$this->arResult['CLIENT_FULL_NAME'] = $this->getClientFullName($client['CONTACT_ID']);
+
+		$this->arResult['IS_CREDIT'] = Credit\Repository::isExistActive($this->arParams['DEAL_ID']); // есть ли активный кредит в сделке
+
+		global $GLOBALS;
+
+		$groupsThatCan = [AGENTS_USER_GROUP_ID, MAIN_ADMIN_USER_GROUP_ID, FINANCE_USER_GROUP_ID, ADMIN_USER_GROUP_ID, TEAM_LEADERS_GROUP_ID];
+		$groupList = UserTable::getUserGroupIds($GLOBALS['USER']->GetID());
+		$accessButton = false;
+
+		foreach($groupList as $groupId){
+			if(in_array($groupId, $groupsThatCan)) {
+				$accessButton = true;
+			}
+		}
+
+		$this->arResult['IS_ACCESS_BUTTON_ALL'] = $accessButton;
+
+		$this->includeComponentTemplate();
+
+	}
+}
