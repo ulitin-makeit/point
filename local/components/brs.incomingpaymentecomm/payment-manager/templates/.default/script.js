0599d2ced024e920484c2386ec548fca815c9a8b
Улитин Евгений <55397251+ulitin-makeit@users.noreply.github.com>
2025-12-18 15:26:42 +0300

    Update script.js

--- local/components/brs.incomingpaymentecomm/payment-manager/templates/.default/script.js
+++ local/components/brs.incomingpaymentecomm/payment-manager/templates/.default/script.js
@@ -6,7 +6,7 @@ BX.Brs.IncomingPayment = function () {
 };
 
 BX.Brs.IncomingPayment.prototype.init = function () {
-	
+
 	$('.incoming-payments-section-left form').on('submit', function (event) { event.preventDefault(); });
 
 	$('.incoming-payment-item-settings input[name=PAYMENT_TYPE]').on('click', this.changeDisplayPaymentType);
@@ -15,12 +15,12 @@ BX.Brs.IncomingPayment.prototype.init = function () {
 	this.changePayment = $('.cor-client-choice .cor-client-type-select input[type="radio"]');
 	this.paymentByUser = $('#paymentByUser');
 	this.paymentByCorp = $('#paymentByCorp');
-	
+
 	this.createAdvancePayment = $('#createAdvancePayment');
 	this.paymentType = $('.incoming-payment-item .selector-wrapper input[type="radio"]');
 	this.pointRateDisplay = $('.incoming-payment-point-rate');
 	this.pointRateValue = $('.incoming-payment-point-rate-value');
-	
+
 	this.containerPaymentType = $('.incoming-payment-item .selector-wrapper > div > div');
 	this.paymentTypeCard = $('#paymentImmediatelyRadio');
 	this.paymentTypeLink = $('#paymentByLinkRadio');
@@ -29,7 +29,7 @@ BX.Brs.IncomingPayment.prototype.init = function () {
 	this.transactionSuccess = $('.incoming-payments-history-item.payment-success');
 	this.transactionInvoice = $('.incoming-payments-history-item.payment-success[data-payment-method="INVOICE"]');
 	this.transactionPoint = $('.incoming-payments-history-item.payment-success[data-payment-by-point="1"]');
-	
+
 	this.makeIncomingPaymentButton = $('.make-incoming-payment');
 	this.makeIncomingPaymentLinkButton = $('.make-incoming-payment-link');
 	this.makeIncomingPaymentInvoiceButton = $('.make-incoming-payment-invoice');
@@ -39,43 +39,43 @@ BX.Brs.IncomingPayment.prototype.init = function () {
 	this.makeIncomingPaymentLinkButton.on('click', this.initiatePayment.bind(this, 'makePaymentLink'));
 	this.makeIncomingPaymentInvoiceButton.on('click', this.initiatePayment.bind(this, 'makePaymentInvoice'));
 	this.makeIncomingPaymentPointButton.on('click', this.initiatePaymentPoint.bind(this, Number(this.makeIncomingPaymentPointButton.attr('data-deal-id')), Number(this.makeIncomingPaymentPointButton.attr('data-contact-id'))));
-	
+
 	this.copyLinkButton = $('.payment-link .fa-copy').on('click', this.copyPaymentLink.bind(this));
-	
+
 	this.changePayment.on('change', this.changeTypeUserPayment.bind(this)).change(); // настраиваем типы создаваемого платежа
 	this.typePayerPreset(); // предустановленные настройки для типов плательщиков при создании платежа
 	this.createAdvancePayment.on('click', this.changeTypeUserPayment.bind(this));
-	
+
 	setTimeout(this.inputPreset, 300); // предустановленные настройки полей в форме создания платежа
-	
+
 };
 
 /**
  * Получает курс конвертации для указанного типа баллов.
- * 
+ *
  * @param {string} pointType - тип баллов ('mr_rub' или 'imperia_rub')
  * @returns {number|null} - курс конвертации или null если не найден
  */
 BX.Brs.IncomingPayment.prototype.getPointConversionRate = function(pointType) {
 	var rates = window.pointRates;
 	var type = pointType.toUpperCase();
-	
+
 	if (!rates) {
 		return null;
 	}
-	
+
 	if (type === 'MR_RUB') {
 		return rates.MR_RATE || null;
 	} else if (type === 'IMPERIA_RUB') {
 		return rates.IMPERIA_RATE || null;
 	}
-	
+
 	return null;
 };
 
 /**
  * Обновляет отображение курса баллов при смене типа.
- * 
+ *
  * @param {string} pointType - тип баллов ('mr_rub' или 'imperia_rub')
  */
 BX.Brs.IncomingPayment.prototype.updatePointRateDisplay = function(pointType) {
@@ -83,30 +83,30 @@ BX.Brs.IncomingPayment.prototype.updatePointRateDisplay = function(pointType) {
 		this.pointRateDisplay.hide();
 		return;
 	}
-	
+
 	var rate = this.getPointConversionRate(pointType);
-	
+
 	if (rate === null) {
 		this.pointRateDisplay.hide();
 		return;
 	}
-	
+
 	var typeName = pointType.toUpperCase() === 'MR_RUB' ? 'MR' : 'Imperia';
-	
+
 	this.pointRateValue.text(rate + ' руб. за 1 балл ' + typeName);
 	this.pointRateDisplay.show();
 };
 
 /**
  * Предустановленные настройки полей.
- * 
+ *
  * @returns {undefined}
  */
 BX.Brs.IncomingPayment.prototype.inputPreset = function(){
-	
+
 	let isCredit = $('#changeDealPayTypeCredit').prop('checked');
 	let maxAmount = 9999999999;
-	
+
 	$('.incoming-payment-item input[name=AMOUNT_POINT]').inputmask(
 		'currency',
 		{
@@ -117,11 +117,11 @@ BX.Brs.IncomingPayment.prototype.inputPreset = function(){
 			nullable: false
 		}
 	);
-	
+
 	if(isCredit){
 		maxAmount = Number($('.payments-owes-rub').attr('data-amount-debt'))*-1;
 	}
-	
+
 	$('.incoming-payment-item input[name=AMOUNT]').inputmask(
 		'currency',
 		{
@@ -136,51 +136,51 @@ BX.Brs.IncomingPayment.prototype.inputPreset = function(){
 
 /**
  * Предустановленные настройки по типам плательщика.
- * 
+ *
  * @returns {undefined}
  */
 BX.Brs.IncomingPayment.prototype.typePayerPreset = function(){
 	if(this.transactionInvoice.length > 0){ // если есть хоть один платёж с типом "Оплата по счёту", то запрещаем выбирать клиента и выбираем корп. плательщика
-		
+
 		this.paymentByCorp.click();
-		
+
 		this.paymentByUser.parent().hide(0);
-		
+
 	} else if(this.transactionSuccess.length > 0){ // если есть хоть один успешный платёж и нет платежа с типом "Оплата по счёту", то запрещаем выбирать корп. плательщика и выбираем клиента
-	
+
 		this.paymentByUser.click();
-		
+
 		this.paymentByCorp.parent().hide(0);
-		
+
 	}
 }
 
 /**
  * Осуществляем настройку типов создаваемого платежа на основе выбранного типа плательщика в платеже.
- * 
+ *
  * @returns {undefined}
  */
 BX.Brs.IncomingPayment.prototype.changeTypeUserPayment = function(){
-	
+
 	if(this.paymentByUser.prop('checked')){ // если плательщик "Клиент" из сделки
-		
+
 		this.containerPaymentType.show(0);
 		this.paymentTypeInvoice.parent().hide(0);
 		this.paymentTypeCard.click();
-		
+
 		if(typeof window.accessAdvancePaymentType != 'undefined'){
 			this.setAccessPaymentType(window.accessAdvancePaymentType, false);
 		}
-		
+
 	} else { // если плательщик компания из "Место работы" в клиенте (контакте)
-	
+
 		this.containerPaymentType.hide(0);
-		
+
 		this.paymentTypeInvoice.parent().show(0);
 		this.paymentTypeInvoice.click();
-		
+
 	}
-	
+
 };
 
 BX.Brs.IncomingPayment.prototype.changeTypeFinancialCard = function (event) {
@@ -189,23 +189,23 @@ BX.Brs.IncomingPayment.prototype.changeTypeFinancialCard = function (event) {
 };
 
 BX.Brs.IncomingPayment.prototype.makePaymentStrategy = function (strategy, formData) {
-	
+
 	if (this[strategy]) {
 		this[strategy](formData);
 	} else {
 		console.error('Тип оплаты [' + strategy + '] не найден');
 	}
-	
+
 };
 
 BX.Brs.IncomingPayment.prototype.changeDisplayPaymentType = function () {
-	
+
 	var infoPoint = $('.incoming-payment-item-info-point');
 	var buttonAmount = $('.incoming-payment-item-left-amount');
 	var buttonPointAmount = $('.incoming-payment-item-left-point-amount');
 	var left = $('.incoming-payment-item-left');
 	var incoming = $('.incoming-payment-item-right');
-	
+
 	incoming
 		.removeClass('incoming-payment-type-card')
 		.removeClass('incoming-payment-type-payment-link')
@@ -230,19 +230,19 @@ BX.Brs.IncomingPayment.prototype.changeDisplayPaymentType = function () {
 		incoming.addClass('incoming-payment-type-payment-invoice');
 		left.addClass('incoming-payment-type-payment-invoice');
 	}
-	
+
 	if ($(this).val() === 'point') {
-		
+
 		incoming.addClass('incoming-payment-type-point');
 		left.addClass('incoming-payment-type-point');
-		
+
 		infoPoint.show(0);
-		
+
 		buttonPointAmount.show(0);
 		buttonAmount.hide(0);
-		
+
 	}
-	
+
 };
 
 BX.Brs.IncomingPayment.prototype.initiatePayment = function (paymentTypeAction, event) {
@@ -275,91 +275,91 @@ BX.Brs.IncomingPayment.prototype.initiatePayment = function (paymentTypeAction, 
 };
 
 BX.Brs.IncomingPayment.prototype.initiatePaymentPoint = function (dealId, contactId, event) {
-	
+
 	event.preventDefault();
 
 	var formData = {};
-	
+
 	$(event.currentTarget).closest('form').serializeArray().forEach(function (item) {
 		formData[item['name']] = item['value'];
 	});
-	
+
 	// Проверка что тип баллов выбран
 	if (!formData['POINT_TYPE'] || formData['POINT_TYPE'] === '') {
 		this.wrong('Необходимо выбрать тип баллов.');
 		return;
 	}
-	
+
 	var pointMaxAmount = parseFloat($('.crm_entity_field_point_' + formData['POINT_TYPE'].replace('_rub', '') + ':first').text());
 
 	if (contactId == 0) {
 		this.wrong('Клиент должен быть прикреплён к сделке.');
 		return;
 	}
-	
+
 	if (!formData['AMOUNT_POINT'] || formData['AMOUNT_POINT'] <= 0) {
 		this.wrong('Сумма платежа должна быть больше нуля.');
 		return;
 	}
-	
+
 	let amount = formData['AMOUNT_POINT'];
-	
+
 	if(amount > 0){
-			
+
 		let rate = this.getPointConversionRate(formData['POINT_TYPE']);
-	
+
 		if (rate === null) {
 			this.wrong('Не удалось получить курс баллов для выбранного типа.');
 			return;
 		}
 
-		amount = Math.ceil(amount * rate);
-		
+		amount = Math.ceil(amount / rate);
+
 	}
-	
+
 	if (Number(formData['AMOUNT_POINT']) > pointMaxAmount) {
 		this.wrong('Указанная сумма баллов превышает количество баллов на бонусном счёте клиента.');
 		return;
 	}
-	
-	var amountRub = parseFloat(formData['AMOUNT_POINT']);
+
+	var amountRub = formData['AMOUNT'];
 	this.makePaymentPoint(dealId, contactId, formData['POINT_TYPE'], amountRub, amount);
-	
+
 };
 
 /**
  * Настраиваем доступ к типу "Оплата баллами" в создаваемом платеже.
- * 
+ *
  * @param {type} access параметры доступа
  * @param {type} isTimeout нужен таймаут или нет
  * @returns {undefined}
  */
 BX.Brs.IncomingPayment.prototype.setAccessPaymentType = function(access, isTimeout = true){
-	
+
 	window.accessAdvancePaymentType = access;
-	
+
 	if(isTimeout){
 		setTimeout(this.setAccessPaymentTypePoint.bind(this, access), 500);
 	} else {
 		this.setAccessPaymentTypePoint(access);
 	}
-	
+
 };
 
 /**
  * Настраиваем доступ к типу "Оплата баллами" и доступ к типу выбираемых баллов в создаваемом платеже. Либо разрешаем выбирать этот тип, либо запрещаем.
- * 
+ *
  * @param {type} access
  * @returns {undefined}
  */
 BX.Brs.IncomingPayment.prototype.setAccessPaymentTypePoint = function(access){
-	
+
 	if(access.point == true){ // если разрешено создавать платежи с баллами
 
 		$('#paymentImmediatelyRadio, #paymentByLinkRadio, #paymentByInvoice').parent().hide(0); // запрещаем создавать платежи по картам, ссылке и счёту
 
 		$('#paymentPointRadio').click(); // выбираем тип платежа "Оплата баллами"
-		
+
 		access.pointProgramCode = (access.pointProgramCode).replace('IR', 'imperia');
 
 		window.pointPaymentOptionValue = (access.pointProgramCode).toLowerCase() + '_rub';
@@ -369,80 +369,80 @@ BX.Brs.IncomingPayment.prototype.setAccessPaymentTypePoint = function(access){
 				$(this).remove();
 			}
 		});
-		
+
 		if($('#paymentByCorp').length > 0){
 			$('#paymentByCorp').parent().hide();
 		}
 
-	} else {	
-		$('#paymentPointRadio').parent().hide(0); // скрываем возможность выбора типа платежа "Оплата баллами"		
+	} else {
+		$('#paymentPointRadio').parent().hide(0); // скрываем возможность выбора типа платежа "Оплата баллами"
 	}
-	
+
 };
 
 BX.Brs.IncomingPayment.prototype.setPointAmount = function (response){
-	
+
 	BX.Brs.IncomingPayment.prototype.setInputPointAmount(response);
-	
+
 	$('.payment-point select[name="POINT_TYPE"]').on('change', BX.Brs.IncomingPayment.prototype.setInputPointAmount.bind(this, response));
-	
+
 };
 
 BX.Brs.IncomingPayment.prototype.setInputPointAmount = function (response){
-	
+
 	var inputAmountPoint = $('.incoming-payment-input-item input[name="AMOUNT_POINT"]');
 	var domAmountDebp = $('.payments-owes-rub[data-amount-debt]');
-	
+
 	var inputPointType = $('.incoming-payment-type-point select[name="POINT_TYPE"]');
-	
+
 	var pointType = inputPointType.val();
-	
+
 	// Если тип не выбран — очищаем поле суммы и выходим
 	if(!pointType || pointType === ''){
 		inputAmountPoint.val('');
 		return;
 	}
-	
+
 	pointType = pointType.toUpperCase();
-	
+
 	var amountDebt = parseFloat(domAmountDebp.attr('data-amount-debt'));
 	var amount = parseFloat(response.data[pointType]);
-	
+
 	if(amountDebt < 0){
-		
+
 		amountDebt = amountDebt * -1;
-		
+
 		if(amountDebt < amount && (amount - amountDebt) == 0){
 			amount = amount - amountDebt;
 		} else if(amountDebt < amount && (amount - amountDebt) > 0){
 			amount = amountDebt;
 		}
-		
+
 		if(amount > 0){
-			
+
 			let rate = BX.Brs.IncomingPayment.prototype.getPointConversionRate(pointType);
 
 			if (rate === null) {
 				this.wrong('Не удалось получить курс баллов для выбранного типа.');
 				return;
 			}
-			
-			amount = Math.ceil(amount * rate);
-			
+
+			amount = Math.ceil(amount / rate);
+
 		}
-		
+
 	} else {
 		amount = 0;
 	}
-	
+
 	inputAmountPoint.val(amount);
-	
+
 }
 
 BX.Brs.IncomingPayment.prototype.makePaymentPoint = function (dealId, contactId, rateType, amountRub, amountPoints){
-	
+
 	BX.financeCardTabManager.showLoading();
-	
+
 	// отправляем запрос в контроллер списания баллов модуля "brs.point"
 	BX.ajax.runAction('brs:point.api.PointPaymentController.run', {
 		data: {
@@ -459,13 +459,13 @@ BX.Brs.IncomingPayment.prototype.makePaymentPoint = function (dealId, contactId,
 		if(response.data.length === 0){
 			return;
 		}
-		
+
 	}.bind(this), (response) => {
-	
+
 		BX.financeCardTabManager.hideLoading();
-		
+
 		let errMess = 'Произошла ошибка при совершении оплаты баллами<hr>';
-		
+
 		for (let errKey in response.errors) {
 			errMess += response.errors[errKey]['message'] + '<hr>';
 		}
@@ -485,7 +485,7 @@ BX.Brs.IncomingPayment.prototype.makePaymentPoint = function (dealId, contactId,
 		popUp.show();
 
 	});
-	
+
 };
 
 /**
@@ -598,11 +598,11 @@ BX.Brs.IncomingPayment.prototype.makePaymentLink = function (formData) {
 };
 
 BX.Brs.IncomingPayment.prototype.makePaymentInvoice = function (formData) {
-	
+
 	BX.financeCardTabManager.showLoading();
 
 	const params = this.prepareParams();
-	
+
 	if (!params) {
 		return;
 	}
@@ -611,27 +611,27 @@ BX.Brs.IncomingPayment.prototype.makePaymentInvoice = function (formData) {
 		'brs:incomingpaymentecomm.api.PayController.initiatePaymentInvoice',
 		{
 			data: {
-				
+
 				amount: formData['AMOUNT'],
 				dealId: $('#financialCardDealId').val(),
-				
+
 				options: {
 					clientScheme: params.clientScheme
 				}
-				
+
 			}
 		}
 	);
 
 	request.then(response => {
-		
+
 		$('#btnCloseDeal').addClass('ui-btn-disabled');
-		
+
 		BX.financeCardTabManager.refreshTab();
 		BX.financeCardTabManager.hideLoading();
-		
+
 	});
-	
+
 };
 
 BX.Brs.IncomingPayment.prototype.wrong = function (message) {
